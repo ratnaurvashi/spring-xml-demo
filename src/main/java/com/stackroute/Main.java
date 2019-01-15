1545f1a4048a672f473ccf4ec4e05ba98245f327
urvashi.ratna <urvashi.ratna@boeing.com>
2019-01-15 23:38:51 +0530

    task4

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,27 +14,27 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
     public static void main(String[] args){
 
-        //Using XmlBeanFactory
-        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie1=(Movie) beanFactory.getBean("movie1");
-        System.out.println("Using XmlBeanFactory: "+ movie1.getActor());
-
-        //Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
-        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie2=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie1");
-        System.out.println("Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader: "+movie2.getActor());
-
+//        //Using XmlBeanFactory
+//        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+//        Movie movie1=(Movie) beanFactory.getBean("movie1");
+//        System.out.println("Using XmlBeanFactory: "+ movie1.getActor());
+//
+//        //Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
+//        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
+//        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+//        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+//        Movie movie2=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie1");
+//        System.out.println("Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader: "+movie2.getActor());
+//
         //Using ApplicationContext
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie3=(Movie) context.getBean("movie1");
         System.out.println("Using ApplicationContext: "+ movie3.getActor());
 
-        Movie movie4=(Movie) context.getBean("movie2");
-        System.out.println(movie4.getActor());
-
-        Movie movie5=(Movie) context.getBean("movie3");
-        System.out.println(movie5.getActor());
+//        Movie movie4=(Movie) context.getBean("movie2");
+//        System.out.println(movie4.getActor());
+//
+//        Movie movie5=(Movie) context.getBean("movie3");
+//        System.out.println(movie5.getActor());
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     private Actor actor;
 
     public Movie(){
@@ -24,5 +31,19 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Actor actorA= (Actor) applicationContext.getBean("actor2");
+        System.out.println("from application context aware "+actorA.getName());
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        Actor actorB= (Actor) beanFactory.getBean("actor2");
+        System.out.println("From Bean Factory "+actorB.getName());
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("Bean name is "+s);
     }
 }
